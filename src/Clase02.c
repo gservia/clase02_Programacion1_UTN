a3880fc8bfda30d851eeee160f29807d19ab703e
Gabriel <gaby_servia@hotmail.com>
2021-08-26 21:42:39 -0300

    Primer commit

---
+++ src/Clase02.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+	// Windows
+	setbuf(stdout,NULL); // Limpia el buffer de la consola
+
+	int a;
+	float f;
+	char c;
+
+	printf("Dame un numero:\n");
+
+	fflush(stdin); // Limpia el buffer antes de un scanf
+
+	scanf("%d",&a);
+
+	printf("Tu numero es: %d",a);
+
+
+
+
+	return EXIT_SUCCESS;
+}
